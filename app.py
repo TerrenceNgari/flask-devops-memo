cf70ea1dc2aa58ff80745931e0f10309e52494fe
TerrenceNgari <117580533+TerrenceNgari@users.noreply.github.com>
2026-02-25 04:40:50 +0000

    Add new endpoints

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "DevOps is working!"
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "healthy"})
+
+@app.route("/api/version")
+def version():
+    return jsonify({
+        "app": "flask-devops-demo",
+        "version": "1.0.0"
+    })
+
+@app.route("/api/users")
+def users():
+    return jsonify([
+        {"id": 1, "name": "Terrence"},
+        {"id": 2, "name": "DevOps Engineer"}
+    ])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
