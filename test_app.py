af87755ec8faac5754a1ec7e869727875c3b9c5d
TerrenceNgari <117580533+TerrenceNgari@users.noreply.github.com>
2026-02-25 06:25:48 +0000

    Update test to match new home page message

--- test_app.py
+++ test_app.py
@@ -3,5 +3,5 @@ from app import app
 def test_home():
     client = app.test_client()
     response = client.get("/")
-    assert response.data == b"DevOps is working!"
+    assert response.data == b"NEW VERSION DEPLOYED"
     
